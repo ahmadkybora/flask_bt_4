0f5baf2d071ac724e14b474026de9e99a8c87f76
ahmadkybora <ahmadkybora@gmail.com>
2022-02-18 21:19:43 +0330

    =new

--- index.py
+++ index.py
@@ -1,26 +1,40 @@
-from asyncore import dispatcher
-from turtle import update
-from telegram import Update,ReplyKeyboardMarkup,ReplyKeyboardRemove,Bot,InlineKeyboardButton,InlineKeyboardMarkup,KeyboardButton,CallbackQuery,ParseMode
-from telegram.ext import CommandHandler,Updater,Dispatcher,MessageHandler,Filters,CallbackContext,CallbackQueryHandler
 import logging
-import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-# token = os.getenv('TOKEN')
-token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
 
-def start(update: Update,context: CallbackContext):
-    update.message.reply_text("hi")
+
+def start(update, context):
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def main():
+    token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
     updater = Updater(token, use_context=True)
-    bot = Bot(token)
-    dispatcher: Dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
