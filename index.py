1ce86f3b1a9d7a5d5409239cc338a2acf9fe5f6e
ahmadkybora <ahmadkybora@gmail.com>
2022-02-18 21:44:19 +0330

    =new

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
-from telegram import Update,ReplyKeyboardMarkup,ReplyKeyboardRemove,Bot,InlineKeyboardButton,InlineKeyboardMarkup,KeyboardButton,CallbackQuery,ParseMode
-from telegram.ext import CommandHandler,Updater,Dispatcher,MessageHandler,Filters,CallbackContext,CallbackQueryHandler
 import logging
-import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
 token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
 
 def start(update, context):
@@ -19,7 +17,7 @@ def help(update, context):
 
 
 def contact_us(update, context):
-    update.message.reply_text("Contact us")
+    update.message.reply_text('contact us!')
 
 
 def echo(update, context):
@@ -33,15 +31,6 @@ def error(update, context):
 def main():
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
-
-    keyboard = [
-        [KeyboardButton('Start')],
-        [KeyboardButton('Contact us')],
-        [KeyboardButton('Help')], 
-        [KeyboardButton('File')]
-    ]
-    key = ReplyKeyboardMarkup(keyboard,resize_keyboard=True)
-
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("contact us", contact_us))
